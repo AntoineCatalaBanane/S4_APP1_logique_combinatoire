16a64693a77dfe1d5e857d666f80ac932d3a6e54
tfraude <tristanfraud@icloud.com>
2026-05-10 16:25:18 -0400

    Thermo2bin

--- pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin.vhd
+++ pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin.vhd
@@ -38,8 +38,48 @@ entity Thermo2Bin is
 end Thermo2Bin;
 
 architecture Behavioral of Thermo2Bin is
+signal temp : std_logic_vector(3 downto 0);
+signal erreur : std_logic;
 
 begin
+
+temp(0) <= (ADCth(0) and not ADCth(1)) or
+           (ADCth(2) and not ADCth(3)) or
+           (ADCth(4) and not ADCth(5)) or
+           (ADCth(6) and not ADCth(7)) or
+           (ADCth(8) and not ADCth(9)) or
+           (ADCth(10) and not ADCth(11));
+           
+temp(1) <= (ADCth(1) and not ADCth(3)) or
+           (ADCth(5) and not ADCth(7)) or
+           (ADCth(9) and not ADCth(11));
+
+temp(2) <= (ADCth(3) and not ADCth(7)) or
+           (ADCth(11));
+ 
+temp(3) <= ADCth(7);
+
+ADCbin <= temp; 
+
+erreur <= (not ADCth(0) and ADCth(1)) or
+         (not ADCth(1) and ADCth(2)) or
+         (not ADCth(2) and ADCth(3)) or
+         (not ADCth(3) and ADCth(4)) or
+         (not ADCth(4) and ADCth(5)) or
+         (not ADCth(5) and ADCth(6)) or
+         (not ADCth(6) and ADCth(7)) or
+         (not ADCth(7) and ADCth(8)) or
+         (not ADCth(8) and ADCth(9)) or
+         (not ADCth(9) and ADCth(10)) or
+         (not ADCth(10) and ADCth(11));          
+
+
+
+    process(ADCth)
+    
+    begin   
+        
+   end process;
 
 
 end Behavioral;
